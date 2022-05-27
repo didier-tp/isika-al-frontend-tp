dd6c72ade734533844d1e6849939c243bfd7e315
DidierDefrance <didier@d-defrance.fr>
2022-05-27 12:06:28 +0200

    autorisations CORS

--- tp-js/myNodeServer/server.js
+++ tp-js/myNodeServer/server.js
@@ -1,5 +1,5 @@
 //var express = require('express');
-import express from 'express' ;
+import express from 'express';
 import * as produitApiRoutes from './produit-api-routes-memory.js';
 import * as deviseApiRoutes from './devise-api-routes-memory.js';
 //import * as deviseApiRoutes from './devise-api-routes-mongoose-cb.js';
@@ -11,15 +11,30 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 var app = express();
 
 //support parsing of JSON post data
-var jsonParser = express.json({  extended: true}); 
+var jsonParser = express.json({ extended: true });
 app.use(jsonParser);
 
+// CORS enabled with express/node-js :
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  //ou avec "www.xyz.com" à la place de "*" en production
+  res.header("Access-Control-Allow-Methods",
+    "POST, GET, PUT, DELETE, PATCH"); //default: GET, ...
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, PATCH, DELETE');
+    //to give access to all the methods provided
+    return res.status(200).json({});
+  }
+  next();
+});
 
 //les routes en /html/... seront gérées par express par
 //de simples renvois des fichiers statiques
 //du répertoire "./html"
-app.use('/html', express.static(__dirname+"/html"));
-app.get('/', function(req , res ) {
+app.use('/html', express.static(__dirname + "/html"));
+app.get('/', function (req, res) {
   res.redirect('/html/index.html');
 });
 
@@ -27,6 +42,6 @@ app.get('/', function(req , res ) {
 app.use(produitApiRoutes.apiRouter);
 app.use(deviseApiRoutes.apiRouter);
 
-app.listen(8282 , function () {
+app.listen(8282, function () {
   console.log("http://localhost:8282");
 });
