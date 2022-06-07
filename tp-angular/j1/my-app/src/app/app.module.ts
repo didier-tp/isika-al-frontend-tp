876eb93892d1636d4c6bc03c66cebfb7a66832f6
DidierDefrance <didier@d-defrance.fr>
2022-06-08 00:08:16 +0200

    s

---
+++ tp-angular/j1/my-app/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BasicComponent } from './basic/basic.component';
+import { CalculatriceComponent } from './basic/calculatrice/calculatrice.component';
+import { TvaComponent } from './basic/tva/tva.component';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SeuilComponent } from './demo/seuil/seuil.component';
+import { DemoComponent } from './demo/demo.component';
+import { ZzComponent } from './demo/zz/zz.component';
+import { RegletteComponent } from './demo/reglette/reglette.component';
+import { ListProdComponent } from './demo/list-prod/list-prod.component';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { DeviseComponent } from './devise/devise.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BasicComponent,
+    CalculatriceComponent,
+    TvaComponent,
+    LoginComponent,
+    WelcomeComponent,
+    DemoComponent,
+    SeuilComponent,
+    ZzComponent,
+    RegletteComponent,
+    ListProdComponent,
+    DeviseComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    TabsModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ tp-angular/j2/my-app/src/app/devise/devise.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Devise } from '../common/data/devise';
+
+@Component({
+  selector: 'app-devise',
+  templateUrl: './devise.component.html',
+  styleUrls: ['./devise.component.scss']
+})
+export class DeviseComponent implements OnInit {
+
+  cloneDevise(d:Devise){
+    return JSON.parse(JSON.stringify(d));
+  }
+
+  tabDevises : Devise[] = [];
+
+  selectedDevise : Devise | undefined;
+
+  //[(ngModel)]="deviseTemp.code" , ....
+  deviseTemp : Devise = new Devise();
+
+  message /*: string*/ ="";
+
+  mode : "newOne" | "existingOne" = "newOne";
+
+  constructor() {
+    //V1 (sans backend), avec des valeurs simulées en mémoire
+    this.tabDevises.push(new Devise("EUR","Euro",1));
+    this.tabDevises.push(new Devise("USD","Dollar",1.1));
+    this.tabDevises.push(new Devise("GBP","Livre",0.9));
+    this.tabDevises.push(new Devise("JPY","Yen",120));
+   }
+
+  ngOnInit(): void {
+  }
+
+  //à coder en TP:
+  //onNew() , onAdd() , onDelete() , onUpdate(), onSelectDevise(d : Devise )
+
+}
