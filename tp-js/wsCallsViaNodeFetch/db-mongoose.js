13047fc2e143d2b00b2981e2d70efb27976c4208
DidierDefrance <didier@d-defrance.fr>
2022-07-15 16:54:03 +0200

    s

--- tp-js/wsCallsViaNodeFetch/db-mongoose.js
+++ tp-js/wsCallsViaNodeFetch/db-mongoose.js
@@ -6,6 +6,8 @@ var mongoDbUrl = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017"; //by de
 console.log("mongoDbUrl="+mongoDbUrl);
 mongoose.connect(mongoDbUrl, {useNewUrlParser: true, 
 	                              useUnifiedTopology: true , 
+                                user : "" , pass : "" ,
+                                /*user: "username_telque_superuser" , pass : "motdepasse",*/
 								  dbName : 'meteo'});
 var thisDb  = mongoose.connection;
 
