771499555bb4033aac3e7bc2a092e03ac86f6cb7
DidierDefrance <didier@d-defrance.fr>
2022-09-27 13:52:05 +0200

    localStorage

--- tp-js/web/js/testStorage.js
+++ tp-js/web/js/testStorage.js
@@ -2,5 +2,22 @@ function memoriserCouleur(){
     let eltCouleur = document.getElementById("couleur");
     let valCouleur = eltCouleur.value;
     localStorage.setItem("couleurPreferee" ,valCouleur );
-    //...
+    let userSession = { name : "toto" , role : "developpeur" };
+    sessionStorage.setItem("userSession" , JSON.stringify(userSession) );
+    init();
+}
+
+window.onload = init;
+
+function init(){
+    let valCouleur = localStorage.getItem("couleurPreferee");
+    if(valCouleur!=null){
+       let  eltBody = document.querySelector("body");
+        eltBody.style.backgroundColor = valCouleur;
+    }
+    let userSessionAsJsonString = sessionStorage.getItem("userSession");
+    if(userSessionAsJsonString!=null){
+        let  userSession = JSON.parse(userSessionAsJsonString);
+        document.getElementById("spanMessage").innerHTML = userSession.name;
+    }
 }
