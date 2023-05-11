ee7140e8caf20dfce17020e6dcdac259af8ff474
DidierDefrance <didier@d-defrance.fr>
2023-05-11 15:39:32 +0200

    s

--- appelAjaxExterieurAuProjet/js/my_ajax_util.js
+++ appelAjaxExterieurAuProjet/js/my_ajax_util.js
@@ -2,21 +2,29 @@
 function registerCallbacks(xhr,callback,errCallback) {
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4){
-			if((xhr.status == 200 || xhr.status == 0)) {
-				callback(xhr.responseText);
+			if((xhr.status == 200 || xhr.status == 204 ||  xhr.status == 201)) {
+				//200: OK , 201: created (after good POST) , 204 : NoContent after good delete
+				callback(xhr.responseText,xhr.status);
 			}
 			else {
 				if(errCallback)
-				     errCallback(xhr.responseText);
+				     errCallback(xhr.responseText,xhr.status);
 			}
 		}
 	};
+}
+
+function injectAuthBearerTokenInRequestHeader(xhrReq){
+	authToken = sessionStorage.getItem("authToken");
+	if(authToken!=null && authToken!="")
+	    xhrReq.setRequestHeader('Authorization', 'Bearer ' + authToken);
 }
 
 function makeAjaxGetRequest(url,callback,errCallback) {
 	var xhr = new XMLHttpRequest();
 	registerCallbacks(xhr,callback,errCallback);
 	xhr.open("GET", url, true);
+	injectAuthBearerTokenInRequestHeader(xhr);
 	xhr.send(null);
 }
 
@@ -24,6 +32,7 @@ function makeAjaxDeleteRequest(url,callback,errCallback) {
 	var xhr = new XMLHttpRequest();
 	registerCallbacks(xhr,callback,errCallback);
 	xhr.open("DELETE", url, true);
+	injectAuthBearerTokenInRequestHeader(xhr);
 	xhr.send(null);
 }
 
@@ -32,6 +41,7 @@ function makeAjaxPostRequest(url,jsonData,callback,errCallback) {
 	registerCallbacks(xhr,callback,errCallback);
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-Type", "application/json");
+	injectAuthBearerTokenInRequestHeader(xhr);
 	xhr.send(jsonData);
 }
 
@@ -41,6 +51,7 @@ function makeAjaxPostMultipartRequest(url,formData,callback,errCallback) {
 	xhr.open("POST", url, true);
 	//xhr.setRequestHeader("Content-Type","multipart/form-data; boundary=....."); 
 	//par defaut le "Content-Type" est bien renseigné avec bon boundary
+	injectAuthBearerTokenInRequestHeader(xhr);
 	xhr.send(formData);
 }
 
@@ -49,5 +60,6 @@ function makeAjaxPutRequest(url,jsonData,callback,errCallback) {
 	registerCallbacks(xhr,callback,errCallback);
 	xhr.open("PUT", url, true);
 	xhr.setRequestHeader("Content-Type", "application/json");
+	injectAuthBearerTokenInRequestHeader(xhr);
 	xhr.send(jsonData);
 }
