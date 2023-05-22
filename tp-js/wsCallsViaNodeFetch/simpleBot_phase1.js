ed80bf4ff2131052b8b52b70f9e2e0bba65b8117
DidierDefrance <didier@d-defrance.fr>
2023-05-22 16:35:54 +0200

    s

--- tp-js/wsCallsViaNodeFetch/simpleBot_phase1.js
+++ tp-js/wsCallsViaNodeFetch/simpleBot_phase1.js
@@ -60,12 +60,49 @@ phase 3 :
 
 //console.log("villesAvecMeteo="+ JSON.stringify(villesAvecMeteo));
 
-async function recupererLatLong(tabVilles){
+async function myJsonFetch(wsUrl){
+    try{
+        const response  =  await fetch(wsUrl);
+        //console.log("response.status : ", + response.status);
+        const resObj = await response.json();
+        //console.log("response data : " + JSON.stringify(resObj));
+        return resObj; //NB: in async function value is return as Promise.resolve
+     }catch(ex){
+       console.log("ex : " + ex);
+       throw ex; //NB: in async function exception is thrown as Promise.reject
+     }
+}
 
+async function recupererLatLong(tabVilles){
+    try{
+        for(let ville of tabVilles){
+            const wsUrl= `http://api.zippopotam.us/fr/${ville.zip}`;
+            const resWs = await myJsonFetch(wsUrl);
+            ville.lat=resWs.places[0].latitude;
+            ville.lon=resWs.places[0].longitude;
+        }
+        return tabVilles;//NB: in async function value is return as Promise.resolve
+    }catch(ex){
+        console.log("ex : " + ex);
+        throw ex; //NB: in async function exception is thrown as Promise.reject
+    }
 }
 
 async function recupererInfosMeteo(tabVilles){
-    
+    try{
+        for(let ville of tabVilles){
+            let apiKey = "27a68278aebee75af9d4fc23d7a68f75";//de didier-defrance (formateur fou)
+            const wsUrl= `https://api.openweathermap.org/data/2.5/weather?lat=${ville.lat}&lon=${ville.lon}&appid=${apiKey}`;
+            const resWs = await myJsonFetch(wsUrl);
+            ville.weather_description=resWs.weather[0].description;
+            const tempKelvin = resWs.main.temp;
+            ville.temperature= tempKelvin -273,15 ;//conversion degre kelvin en degré celsius
+        }
+        return tabVilles;//NB: in async function value is return as Promise.resolve
+    }catch(ex){
+        console.log("ex : " + ex);
+        throw ex; //NB: in async function exception is thrown as Promise.reject
+    }
 }
 
 async function recuperLatLongEtInfosMeteo(tabVilles){
@@ -77,3 +114,5 @@ async function recuperLatLongEtInfosMeteo(tabVilles){
         console.log(ex);
     }
 }
+
+recuperLatLongEtInfosMeteo(villesAvecMeteo);
