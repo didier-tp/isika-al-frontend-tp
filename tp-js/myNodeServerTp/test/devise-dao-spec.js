fd90318cd2352a87f86fd6653e8f146770f4b235
DidierDefrance <didier@d-defrance.fr>
2023-05-26 16:47:34 +0200

    s

--- tp-js/myNodeServerTp/test/devise-dao-spec.js
+++ tp-js/myNodeServerTp/test/devise-dao-spec.js
@@ -5,18 +5,22 @@ var PersistentDeviseModel = deviseDao.ThisPersistentModel;
 
 describe("test sur dao devise" , () =>{
 
+    before(async ()=>{
+        let nouvelleDevise = new PersistentDeviseModel({ code : "m2" , change : 2.2 , name : "monnaie2" });
+        await nouvelleDevise.save();
+    });
 
-    it("test lecture devise euro" , async ()=>{
-        let deviseEuro = await PersistentDeviseModel.findById("EUR");
+
+    it("test lecture devise m2" , async ()=>{
+        let deviseEuro = await PersistentDeviseModel.findById("m2");
         console.log("deviseEuro="+ JSON.stringify(deviseEuro));
-        expect(deviseEuro.name).to.equal("Euro");
+        expect(deviseEuro.name).to.equal("monnaie2");
     })
 
-    it("test lecture devise euro" , async ()=>{
-        let deviseEuro = await PersistentDeviseModel.findById("USD");
-        console.log("deviseEuro="+ JSON.stringify(deviseEuro));
-        expect(deviseEuro.name).to.equal("Dollar");
-    })
+    
+    after(async ()=>{
+        await PersistentDeviseModel.deleteOne ({ _id : "m2"});
+    });
 
 
 
